3e56644bc4f6d24482d521bbea7789e44d1dee9d
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-23 12:03:01 +0600

    all ok without localstorage

--- Cash-Maintaining-List-App-main/script.js
+++ Cash-Maintaining-List-App-main/script.js
@@ -5,7 +5,8 @@ var DateId = document.getElementById("dateId");
 var timeId = document.getElementById("timeId");
 const purposeId = document.getElementById("purposeId");
 const ButtonId = document.getElementById("btnId");
-// console.log(DateId.value);
+const tableMain = document.getElementById("main-table");
+const tableBody = document.getElementById("table-body");
 
 const currentDate = new Date();
 DateId.value = currentDate.toDateString();
@@ -13,9 +14,25 @@ DateId.value = currentDate.toDateString();
 const currentTime = new Date();
 timeId.value = currentTime.toTimeString();
 
+var amountIdArray = [];
+
+var responsiveValues = 0;
+
+// function dynamicValue(value)
+// {
+//     for(let i = 0; i < value.length; i++)
+// }
+
 ButtonId.addEventListener("click", () => {
-    console.log(SenderId.value);
-    console.log(receiverId.value);
-    console.log(amountId.value);
-    console.log(purposeId.value);
+    var newTableRow = document.createElement("tr");
+    newTableRow.innerHTML = `<th scope="row">${amountIdArray.length + 1}</th>
+                        <td>${SenderId.value}</td>
+                        <td>${receiverId.value}</td>
+                        <td>${DateId.value}</td>
+                        <td>${timeId.value}</td>
+                        <td>${purposeId.value}</td>
+                        <td>${amountId.value}</td>`;
+    amountIdArray.push(amountId.value);
+    tableBody.appendChild(newTableRow);
+    console.log(amountIdArray.length);
 });
