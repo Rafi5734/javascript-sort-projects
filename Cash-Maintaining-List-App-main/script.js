d89884642b0d357a014364ad209d66872c041e66
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-23 17:40:11 +0600

    try to fix the row of data

--- Cash-Maintaining-List-App-main/script.js
+++ Cash-Maintaining-List-App-main/script.js
@@ -18,21 +18,87 @@ var amountIdArray = [];
 
 var responsiveValues = 0;
 
-// function dynamicValue(value)
-// {
-//     for(let i = 0; i < value.length; i++)
-// }
+const displayCartItems = () => {
+    const cart = getCart();
+
+    for (const itemName in cart) {
+        var newTableRow = document.createElement("tr");
+        newTableRow.innerHTML = `<th scope="row">${
+            amountIdArray.length + 1
+        }</th>
+                            <td scope="row">${itemName}</td>
+                            `;
+        amountIdArray.push(amountId.value);
+        tableBody.appendChild(newTableRow);
+        // console.log(cart[itemName]);
+    }
+    // tableBody.innerHTML = tableData;
+};
 
 ButtonId.addEventListener("click", () => {
-    var newTableRow = document.createElement("tr");
-    newTableRow.innerHTML = `<th scope="row">${amountIdArray.length + 1}</th>
-                        <td>${SenderId.value}</td>
-                        <td>${receiverId.value}</td>
-                        <td>${DateId.value}</td>
-                        <td>${timeId.value}</td>
-                        <td>${purposeId.value}</td>
-                        <td>${amountId.value}</td>`;
-    amountIdArray.push(amountId.value);
-    tableBody.appendChild(newTableRow);
-    console.log(amountIdArray.length);
+    // var newTableRow = document.createElement("tr");
+    // newTableRow.innerHTML = `<th scope="row">${amountIdArray.length + 1}</th>
+    //                     <td>${SenderId.value}</td>
+    //                     <td>${receiverId.value}</td>
+    //                     <td>${DateId.value}</td>
+    //                     <td>${timeId.value}</td>
+    //                     <td>${purposeId.value}</td>
+    //                     <td>${amountId.value}</td>`;
+    // amountIdArray.push(amountId.value);
+    // tableBody.appendChild(newTableRow);
+
+    // if (
+    //     !SenderId.value &&
+    //     !receiverId.value &&
+    //     !purposeId.value &&
+    //     !amountId.value
+    // ) {
+    //     return;
+    // }
+    // displayItems();
+
+    displayCartItems();
+    addToCart(
+        SenderId.value,
+        receiverId.value,
+        DateId.value,
+        purposeId.value,
+        amountId.value
+    );
+    SenderId.value = "";
+    receiverId.value = "";
+    purposeId.value = "";
+    amountId.value = "";
 });
+
+const getCart = () => {
+    const cart = localStorage.getItem("cart");
+    let cartObjs;
+    if (cart) {
+        cartObjs = JSON.parse(cart);
+        console.log(cartObjs);
+    } else {
+        cartObjs = {};
+    }
+    return cartObjs;
+};
+
+const addToCart = (
+    sendName,
+    receiverName,
+    DateTime,
+    purposeName,
+    amountMoney
+) => {
+    const cart = getCart();
+    cart[sendName] = sendName;
+    cart[receiverName] = 1;
+    cart[DateTime] = 1;
+    cart[purposeName] = 1;
+    cart[amountMoney] = 1;
+    const cartToString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartToString);
+    // console.log(cart);
+};
+displayCartItems();
+// localStorageData();
